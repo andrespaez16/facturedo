67d6bf887fe8c03dbded1a792dfc353f788c1773
Andrew paez <andres.paez@packen.co>
2019-12-16 13:03:13 -0500

    create unit test

--- src/app/services/services.service.spec.ts
+++ src/app/services/services.service.spec.ts
@@ -1,12 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ServicesService } from './services.service';
 
-describe('ServicesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: ServicesService = TestBed.get(ServicesService);
-    expect(service).toBeTruthy();
+describe('Http test', () => {
+  let service: ServicesService;
+  beforeEach(async () => {
+      TestBed.configureTestingModule({
+          imports: [HttpClientTestingModule],
+          providers: [
+              ServicesService,
+        
+          ]
+      }).compileComponents();
   });
+  beforeEach(() => {
+      service = TestBed.get(ServicesService);
+  })
+  it('ingreso al app', () => {
+    let credenciales = {
+      identifier: "fernandopc@gamil.com",
+      password:'1234567789',
+  }
+      service.auth(credenciales).subscribe((res:any) => {
+          this.headers = {
+            token: res.headers.get("token"),
+            id: res.headers.get("id"),
+            admin: res.headers.get("admin"),
+            role: res.headers.get("role"),
+            userType: res.headers.get("user_type"),
+          };
+          expect(res).toBeDefined();
+      }, (e: any) => {
+          expect(e);
+      });
+  })
 });
